4d0013caa3ec13ee9008f0e984f7ad3bc5a83421
Martin <rmartin8@una.edu>
2025-08-28 13:31:54 -0500

    add client

--- app.mjs
+++ app.mjs
@@ -1,16 +1,15 @@
 import express from 'express'
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 // import { dirname } from 'node:path';
 // import { fileURLToPath } from 'node:url';
 
 const app = express()
 const PORT = process.env.PORT || 3000; 
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = dirname(__filename);
 
@@ -22,17 +21,22 @@ app.use(express.static(path.join(__dirname + 'public'))); // Serve static files
 // app.use(express.static(__dirname + 'public'));
 app.use(express.static(__dirname + 'public'));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello Express from Render 😍😍😍. <a target="_blank" href="rob">rob</a>')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello Express from Render 😍😍😍. <a target="_blank" href="rob">rob</a>')
+})
 
 // endpoints...middlewares...apis? 
 // send an html file
 
-app.get('/', (req, res) => {
+app.get('/rob', (req, res) => {
   // res.send('rob. <a href="/">home</a>')
   res.sendFile(path.join(__dirname, 'public','rob.html')) 
 
+})
+
+app.get('/api/rob', (req, res) => {
+  const myVar = "Hello from Rob API"
+  res.json({message: myVar})
 })
 
 
