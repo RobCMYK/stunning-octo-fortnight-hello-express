55d2519d0e652e981226a5aedb8816383facdf79
Martin <rmartin8@una.edu>
2025-08-28 12:48:50 -0500

    fixed express static

--- app.mjs
+++ app.mjs
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public')); // Serve static files
 
 // app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 
