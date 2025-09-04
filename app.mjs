1e9ce2caea7204ceab2185299e5691348aa594c0
Martin <rmartin8@una.edu>
2025-09-04 13:21:23 -0500

    html

--- app.mjs
+++ app.mjs
@@ -43,15 +43,15 @@ run().catch(console.dir);
 
 app.use(express.static(path.join(__dirname + 'public'))); // Serve static files
 
+
 app.use(express.json()); // for parsing application/json
 
 
 app.get('/', (req, res) => {
   res.send('Hello Express from Render 😍😍😍. <a href="rob">rob</a>')
+
 })
 
-// endpoints...middlewares...apis? 
-// send an html file
 
 app.get('/rob', (req, res) => {
   // res.send('rob. <a href="/">home</a>')
@@ -59,29 +59,38 @@ app.get('/rob', (req, res) => {
 
 })
 
+
 app.get('/api/rob', (req, res) => {
   const myVar = "Hello from Rob API"
   res.json({message: myVar})
+
 })
+
 
 app.get('/api/query', (req, res) => {
   const name = req.query.name;
   res.json({"message": `Hi ${name}, how are you?`}); 
+
 });
+
 
 app.get('/api/url/:id', (req, res) => {
   console.log("Client responds with URL param:", req.params.id);
   res.json({"message": `Hi ${req.params.id}, how are you?`});
+
 });
+
 
 app.post('/api/body', (req, res) => {
   console.log("Client responds with body:", req.body.name);
   const name = req.body.name;
   res.json({"message": `Hi ${name}, how are you?`});
+
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
+
 })
 
 
